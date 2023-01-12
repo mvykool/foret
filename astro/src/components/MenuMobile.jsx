32a0eaf98555286b80306dbbdfda6934e5450358
maicol <maykolandres4@gmail.com>
2023-01-11 23:47:35 -0500

    added bg and navbar

---
+++ astro/src/components/MenuMobile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {RiMenu5Fill} from 'react-icons/ri'
+
+const MenuMobile = () => {
+  return (
+    <div>
+        <RiMenu5Fill className='text-white border-2 border-[#f7ebb3]  text-xl w-10 h-10  p-1 rounded-lg'/>
+    </div>
+  )
+}
+
+export default MenuMobile
