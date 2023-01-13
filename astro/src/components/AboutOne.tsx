dbec76db85669b152682275df3af69dae7d30332
maicol <maykolandres4@gmail.com>
2023-01-13 10:19:34 -0500

    added to the others elements in the index page

---
+++ astro/src/components/AboutOne.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About-one = () => {
+  return (
+    <div>
+         <h2 className="text-white text-2xl mt-5 font-bold border-b-2 border-[var(--app-second-col)]">About us</h2>
+
+
+ 
+ 
+<p className="text-white text-sm text-center mx-16 mt-16">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis animi distinctio praesentium, sint officiis voluptates. Illum, dolorum. Eos, cum aliquid?</p>
+    </div>
+  )
+}
+
+export default About-one
