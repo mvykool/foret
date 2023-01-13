77d6fb079d99cf66228785919ff03e7c5e178714
maicol <maykolandres4@gmail.com>
2023-01-13 08:02:13 -0500

    added explore page and more images

--- astro/src/components/MenuMobile.jsx
+++ astro/src/components/MenuMobile.jsx
@@ -4,7 +4,7 @@ import {RiMenu5Fill} from 'react-icons/ri'
 const MenuMobile = () => {
   return (
     <div>
-        <RiMenu5Fill className='text-white border-2 border-[#f7ebb3]  text-xl w-10 h-10  p-1 rounded-lg'/>
+        <RiMenu5Fill className='text-white border-2 border-[var(--app-second-col)]  text-xl w-10 h-10  p-1 rounded-lg'/>
     </div>
   )
 }
