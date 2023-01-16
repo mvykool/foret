d78f2a6909787da0a527133c58e14da3ecbd996c
maicol <maykolandres4@gmail.com>
2023-01-16 04:38:30 -0500

    deleted astro iamge

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -6,18 +6,13 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import react from "@astrojs/react";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: netlify()
 });
